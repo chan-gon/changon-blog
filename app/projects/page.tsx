efc28418c6c5fcc39916158a5ef13a598228ac1b
changon <ruggedcafe@gmail.com>
2026-02-18 17:36:59 +0900

    update: update Projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,11 +7,13 @@ export const metadata: Metadata = {
 
 export default function Projects() {
     return (
-        <div className="max-w-4xl py-10">
+        <div className="max-w-4xl">
             <h1 className="text-3xl font-bold mb-6">Projects</h1>
-            <p className="text-lg text-gray-700">
-                In progres
-            </p>
+            <div className="p-6 bg-white border border-gray-200 rounded-lg">
+                <p className="text-lg text-gray-700">
+                    In progress
+                </p>
+            </div>
         </div>
     );
 }
